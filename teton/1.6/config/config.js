67b1236f7fefd3f8943c878fb90a9ddff50b179c
Jose Quemba <joseeliud33@gmail.com.com>
2025-02-15 01:44:38 -0500

    add test_smokeTest.py

--- teton/1.6/config/config.js
+++ teton/1.6/config/config.js
@@ -1,6 +1,6 @@
 const LAT = "43.8866";
 const LON = "-111.6777";
-const APIKEY = "<YOUR API KEY HERE>";
+const APIKEY = "921effc51dd4e2293b4816fb9eb3d222";
 
 const WEATHER_URL_STUB = "./data/weather.json";
 const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
